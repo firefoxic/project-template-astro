0e2d2bc0022b2b986521c154615e17971a85bb57
firefoxic <firefoxic.dev@gmail.com>
2025-08-26 14:23:13 +0300

    Replace `postcss` with `lightningcss`

--- astro.config.js
+++ astro.config.js
@@ -1,7 +1,18 @@
+import { readFileSync } from "node:fs"
+import { fileURLToPath, URL } from "node:url"
+
 import sitemap from "@astrojs/sitemap"
 import { getProjectRoot } from "@firefoxic/utils"
 import { defineConfig } from "astro/config"
+import browserslist from "browserslist"
 import browserslistToEsbuild from "browserslist-to-esbuild"
+import { browserslistToTargets } from "lightningcss"
+
+let queries = readFileSync(`./.browserslistrc`, `utf8`)
+	.split(`\n`)
+	.filter((line) => line !== `` || line.startsWith(`#`))
+	.join(`,`)
+let targets = browserslistToTargets(browserslist(queries))
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,17 +24,28 @@ export default defineConfig({
 	scopedStyleStrategy: `class`,
 	server: {
 		host: true,
+		open: true,
+		// port: 3000,
 	},
 	site: `https://firefoxic.github.io/project-template-astro/`,
 	vite: {
 		build: {
-			assetsInlineLimit: 0,
 			cssMinify: undefined,
 			emptyOutDir: true,
 			target: browserslistToEsbuild(),
 		},
+		resolve: {
+			alias: {
+				'@': fileURLToPath(new URL(`./src`, import.meta.url)),
+			},
+		},
 		css: {
-			transformer: `postcss`,
+			lightningcss: {
+				cssModules: true,
+				minify: false,
+				targets,
+			},
+			transformer: `lightningcss`,
 		},
 	},
 })
