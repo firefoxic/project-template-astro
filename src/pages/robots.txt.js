5d75e143b02e9ee4d7d7eadf413728c14514d976
firefoxic <firefoxic.dev@gmail.com>
2026-03-12 21:17:44 +0300

    Replace `eslint` with `oxlint`

--- src/pages/robots.txt.js
+++ src/pages/robots.txt.js
@@ -8,7 +8,7 @@ Sitemap: ${new URL(`sitemap-index.xml`, import.meta.env.SITE).href}
 /**
  * Handles the GET request for the robots.txt file.
  *
- * @return {Response} A response object with the robots.txt content and appropriate headers.
+ * @returns {Response} A response object with the robots.txt content and appropriate headers.
  */
 export function GET () {
 	return new Response(robotsTxt, {
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,4 +2,4 @@ import { activateNavigation } from "./Navigation.js"
 
 let navigations = document.querySelectorAll(`.Navigation`)
 
-navigations.forEach((navigation) => { activateNavigation(navigation) })
+for (let navigation of navigations) activateNavigation(navigation)
