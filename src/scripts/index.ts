064902f84c5e53e2244bd48226123500f731b705
firefoxic <firefoxic.dev@gmail.com>
2026-03-13 16:05:39 +0300

    Replace bem notation `React` with `two--dashes`

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,5 +1,5 @@
 import { activateNavigation } from "./Navigation.js"
 
-let navigations = document.querySelectorAll(`.Navigation`)
+let navigations = document.querySelectorAll(`.navigation`)
 
 for (let navigation of navigations) activateNavigation(navigation)
