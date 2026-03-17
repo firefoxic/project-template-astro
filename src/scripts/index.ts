cf4c104509f791252753f5d3d1af4b1bf6880fa5
firefoxic <firefoxic.dev@gmail.com>
2026-03-17 09:14:06 +0300

    Reorganize scripts

--- src/scripts/index.ts
+++
@@ -1,5 +0,0 @@
-import { activateNavigation } from "./Navigation.js"
-
-let navigations = document.querySelectorAll(`.navigation`)
-
-for (let navigation of navigations) activateNavigation(navigation)
---
+++ src/scripts/remindMeToDelete.ts
@@ -0,0 +1,6 @@
+const message = `TODO: Delete this example script.`
+
+export function remindMeToDelete (): void {
+	// oxlint-disable-next-line no-console
+	console.info(message)
+}
